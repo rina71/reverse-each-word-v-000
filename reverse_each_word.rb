64d815b3c3c79a7e30f2a8fdd2b49457d6e9a268
Yurina Shin <yurinas@gmail.com>
2017-12-08 22:15:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  setence_reverse = sentence.reverse
- sentence1 = setence_reverse.split(" ")
- sentence2 = sentence1.reverse
-sentence2.join(' ')
+#   setence_reverse = sentence.reverse
+#  sentence1 = setence_reverse.split(" ")
+#  sentence2 = sentence1.reverse
+# sentence2.join(' ')
+sentence.split.each{|word| word.reverse}
 end
