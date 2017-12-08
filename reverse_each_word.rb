1dfb06796bbe84874097b400a0b2020c1b3c0a23
Yurina Shin <yurinas@gmail.com>
2017-12-08 22:16:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(sentence)
 #  sentence1 = setence_reverse.split(" ")
 #  sentence2 = sentence1.reverse
 # sentence2.join(' ')
-sentence.split.map{|word| word.reverse}.join
+sentence.split.map{|word| word.reverse}.join(" ")
 end
